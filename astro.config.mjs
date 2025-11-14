e29137411bb3084e720cf1d793d98d991a00e93a
Johan Bonneau <johan.bonneau@zenika.com>
2025-11-14 01:19:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://zenigata.github.io',
+	base: '/lfi-carquefou-2026',
 	integrations: [
 		starlight({
 			title: 'My Docs',
